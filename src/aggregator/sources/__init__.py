b825e1e587c490045d5fca1ff74a36dcb019b40d
Naitik Gupta <60667035+desenyon@users.noreply.github.com>
2026-02-11 10:29:41 -0800

    refactor: update src/aggregator/sources/__init__.py

--- src/aggregator/sources/__init__.py
+++ src/aggregator/sources/__init__.py
@@ -1,7 +1,6 @@
-# src/aggregator/sources/__init__.py
-
 from .arxiv import ArxivClient
+from .base import BaseSourceClient
 from .pubmed import PubmedClient
 from .semantic_scholar import SemanticScholarClient
 
-__all__ = ['ArxivClient', 'PubmedClient', 'SemanticScholarClient']
+__all__ = ["BaseSourceClient", "ArxivClient", "PubmedClient", "SemanticScholarClient"]
