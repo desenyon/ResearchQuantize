c05a9496318d9d9144d4e902f3ec7e301a77867b
Naitik Gupta <60667035+desenyon@users.noreply.github.com>
2026-02-11 10:29:26 -0800

    refactor: update src/aggregator/models/author.py

--- src/aggregator/models/author.py
+++ src/aggregator/models/author.py
@@ -1,10 +1,9 @@
-# src/aggregator/models/author.py
+from dataclasses import dataclass
+from typing import Optional
 
+
+@dataclass
 class Author:
-    def __init__(self, name, affiliation=None, email=None):
-        self.name = name
-        self.affiliation = affiliation
-        self.email = email
-
-    def __repr__(self):
-        return f"<Author(name='{self.name}', affiliation='{self.affiliation}', email='{self.email}')>"
+    name: str
+    affiliation: Optional[str] = None
+    email: Optional[str] = None
